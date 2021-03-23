ef1f882afac533431cfda5397a3ad3852ec9072d
XPhantomad <adrian.scholze@mailbox.tu-dresden.de>
2021-03-23 19:28:01 +0100

    Rundezahl in obere Leiste bei Spielseite schreiben

--- MafiaApp.Android/obj/Debug/100/android/environment.arm64-v8a.s
+++ MafiaApp.Android/obj/Debug/100/android/environment.arm64-v8a.s
@@ -73,7 +73,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"b024cc46-011e-48b3-80b3-693baf4eaec2"
+	.asciz	"bbd76194-7d87-4dff-a018-97d26888fdbf"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
