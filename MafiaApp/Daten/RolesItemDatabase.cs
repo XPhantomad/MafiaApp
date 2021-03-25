cf0ace58653d5e286e353510c00d3ca98b999499
Adrian <Adrian.Scholze@mailbox.tu-dresden.de>
2021-03-25 13:11:46 +0100

    Rollen Datenbank hinzugefügt

---
+++ MafiaApp/Daten/RolesItemDatabase.cs
@@ -0,0 +1,31 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MafiaApp.Daten
+{
+    public class RolesItemDatabase
+    {
+        static SQLiteAsyncConnection Database;
+
+        public static readonly AsyncLazyusw<RolesItemDatabase> Instance = new AsyncLazyusw<RolesItemDatabase>(async () =>
+        {
+            var inst = new RolesItemDatabase();
+            CreateTableResult result = await Database.CreateTableAsync<PlayerItem>();
+            return inst;
+
+        });
+
+
+        public RolesItemDatabase()
+        {
+            Database = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
+        }
+
+
+
+    }
+}
--- MafiaApp/obj/Debug/netstandard2.0/SettingsPage.xaml.g.cs
+++ MafiaApp/obj/Debug/netstandard2.0/SettingsPage.xaml.g.cs
@@ -17,8 +17,12 @@ namespace MafiaApp {
     public partial class SettingsPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Label playerPresentNumber;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(SettingsPage));
+            playerPresentNumber = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "playerPresentNumber");
         }
     }
 }
